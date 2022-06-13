be66f54a7e986d47fa796b9c8bcc8740fb550a40
Mike Chu <michael.chu@optimizely.com>
2022-06-13 11:11:08 -0400

    Read and parse config

--- src/main.go
+++ src/main.go
@@ -1,8 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+const configJsonPath = "src/config.json"
+
+type Configuration struct {
+	FileExtensionsToUpdate                  []string
+	RelativePathToDisclaimerCommentBlockTxt string
+}
 
 func main() {
-	fmt.Printf("Running...\n")
-	fmt.Println("Exiting...")
+	configuration, readErr := readConfig(configJsonPath)
+	if readErr != nil {
+		fmt.Println("Unable to read or decode config.json")
+		return
+	}
+
+	fmt.Println(configuration.FileExtensionsToUpdate)
+	fmt.Println(configuration.RelativePathToDisclaimerCommentBlockTxt)
+}
+
+func readConfig(configJsonPath string) (*Configuration, error) {
+	file, err := os.Open(configJsonPath)
+
+	if err != nil {
+		fmt.Println("Failed to Open() config.json")
+		return nil, err
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	configuration := Configuration{}
+	err = decoder.Decode(&configuration)
+
+	if err != nil {
+		fmt.Println("Failed to Decode() configuration file")
+		return nil, err
+	}
+
+	return &configuration, err
 }
