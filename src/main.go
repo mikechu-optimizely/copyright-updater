ced6cc15bae92de35917f5dbbab88c11825b2787
Mike Chu <michael.chu@optimizely.com>
2022-06-13 12:11:55 -0400

    WIP stubs to traverse a file dir tree

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
 
 const configJsonPath = "src/config.json"
@@ -11,6 +12,7 @@ const configJsonPath = "src/config.json"
 type Configuration struct {
 	FileExtensionsToUpdate                  []string
 	RelativePathToDisclaimerCommentBlockTxt string
+	RelativeRootDirectoryOfFilesToUpdate    string
 }
 
 func main() {
@@ -20,15 +22,19 @@ func main() {
 		return
 	}
 
-	fmt.Println(configuration.RelativePathToDisclaimerCommentBlockTxt)
+	configuration.FileExtensionsToUpdate = normalizeFileExtensions(configuration.FileExtensionsToUpdate)
+
 	commentBlock, commentBlockError := readCommentBlock(configuration.RelativePathToDisclaimerCommentBlockTxt)
 	if commentBlockError != nil {
 		fmt.Printf("Unable to read comment block from %s", configuration.RelativePathToDisclaimerCommentBlockTxt)
 		return
 	}
-	fmt.Println(commentBlock)
 
-	fmt.Println(configuration.FileExtensionsToUpdate)
+	updateErr := updateFiles(configuration.RelativeRootDirectoryOfFilesToUpdate, configuration.FileExtensionsToUpdate, commentBlock)
+	if updateErr != nil {
+		fmt.Printf("Failed to update files with extensions %s in dir %s", configuration.FileExtensionsToUpdate, configuration.RelativeRootDirectoryOfFilesToUpdate)
+		return
+	}
 }
 
 func readConfig(configJsonPath string) (*Configuration, error) {
@@ -57,6 +63,17 @@ func readConfig(configJsonPath string) (*Configuration, error) {
 	return &configuration, err
 }
 
+func normalizeFileExtensions(fileExtensions []string) []string {
+	for index, extension := range fileExtensions {
+		if strings.HasPrefix(extension, ".") {
+			continue
+		}
+		fileExtensions[index] = "." + extension
+	}
+	fmt.Println(fileExtensions)
+	return fileExtensions
+}
+
 func readCommentBlock(pathToDisclaimerCommentBlock string) (string, error) {
 	fileBytes, err := os.ReadFile(pathToDisclaimerCommentBlock)
 
@@ -65,4 +82,38 @@ func readCommentBlock(pathToDisclaimerCommentBlock string) (string, error) {
 	}
 
 	return string(fileBytes), err
+}
+
+func updateFiles(rootPath string, extensionsToUpdate []string, commentBlock string) error {
+
+	filePath := "src/fakeFile.go"
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("Unable to Open() %s\n", filePath)
+		return err
+	}
+
+	defer func() {
+		closeError := file.Close()
+		if err == nil {
+			err = closeError
+		}
+	}()
+
+	updateFile(file, commentBlock)
+
+	return nil
+}
+
+func updateFile(fileToUpdate *os.File, commentBlock string) {
+	tryRemoveExistingDisclaimer(fileToUpdate)
+	addDisclaimer(fileToUpdate, commentBlock)
+}
+
+func tryRemoveExistingDisclaimer(fileToUpdate *os.File) {
+	fmt.Printf("tryRemoveExistingDisclaimer(%s)\n", fileToUpdate.Name())
+}
+
+func addDisclaimer(fileToUpdate *os.File, block string) {
+	fmt.Printf("addDisclaimer(%s)\n", fileToUpdate.Name())
 }
