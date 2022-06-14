2f8963dd7591ee12416a234e4f2394490db5257c
Mike Chu <michael.chu@optimizely.com>
2022-06-14 15:29:42 -0400

    Add nested test .go and .js

---
+++ testfiles/nested/doublenested/nested-nested-test.js
@@ -0,0 +1,1 @@
+console.log("This is double nested fake file which should receive an updated comment block above.");
---
+++ testfiles/nested/nested_test.go
@@ -0,0 +1,7 @@
+package testfiles
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is a nested fake file which should receive an updated comment block above.")
+}
