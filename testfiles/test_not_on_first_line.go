584197a230f74d2a8411670bf729ba48db0f4506
Mike Chu <michael.chu@optimizely.com>
2022-06-14 17:30:10 -0400

    Add test file whose comment isn't on line 1

---
+++ testfiles/test_not_on_first_line.go
@@ -0,0 +1,11 @@
+/*
+ * This comment won't be replaced since it does not start line 1
+ */
+
+package testfiles
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is just a fake file which should receive an updated comment block above.")
+}
