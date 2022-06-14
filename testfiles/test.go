312202f7fb091c674104765284eb8ca0037fceb0
Mike Chu <michael.chu@optimizely.com>
2022-06-14 11:36:41 -0400

    Add test files in isolated dir

---
+++ testfiles/test.go
@@ -0,0 +1,7 @@
+package testfiles
+
+import "fmt"
+
+func main() {
+	fmt.Println("This is just a fake file which should receive an updated comment block above.")
+}
---
+++ testfiles/test.js
@@ -0,0 +1,1 @@
+console.log("This is just a fake file which should receive an updated comment block above.");
